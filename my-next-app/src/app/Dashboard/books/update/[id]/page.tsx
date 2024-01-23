505480d152a5315c31efd66aefabb0734d4a0f65
weam ibrahim <weamibrahim46@gmail.com>
2024-01-23 22:10:39 +0200

    fix error

--- my-next-app/src/app/Dashboard/books/update/[id]/page.tsx
+++ my-next-app/src/app/Dashboard/books/update/[id]/page.tsx
@@ -3,7 +3,7 @@ import cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function Update({ params }) {
+export default function Update({ params }: { params: { id: string } }) {
   const { id } = params;
   const token = cookies.get("token");
   
