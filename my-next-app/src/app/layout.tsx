744b3bdd6d10ec0d17e0ed10c7abb02a50ec907b
weam ibrahim <weamibrahim46@gmail.com>
2024-01-23 21:43:41 +0200

    fix error

--- my-next-app/src/app/layout.tsx
+++ my-next-app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '../components/header'
 import Footer from '../components/footer'
-
+import 'animate.css';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
