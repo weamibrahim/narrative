9a8b9bf1a2cdbe98742bc237b352de36cb400000
weam ibrahim <weamibrahim46@gmail.com>
2024-01-27 20:20:46 +0200

    update for deploy

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -8,10 +8,7 @@ import { Carousel } from 'flowbite-react';
 import{ useState } from "react";
 
 
-const fetcher = (url: any) => fetch(url,
-                        { 
-                          credentials: 'include',
-                        }).then((res) => res.json());
+const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
 export default function Home() {
 
