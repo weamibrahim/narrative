acfae0b76f8aad82e539d73c6b22d0dad6ccb82d
weam ibrahim <weamibrahim46@gmail.com>
2024-08-23 19:41:23 +0300

    update link

--- my-next-app/src/components/header.tsx
+++ my-next-app/src/components/header.tsx
@@ -39,7 +39,9 @@ function Header() {
         <div className="flex items-center space-x-3 rtl:space-x-reverse">
           <IoBook className="text-2xl text-fuchsia-700" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white" style={{ fontFamily: 'cursive' }}>
-            Narrative
+          <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/">
+               Narrative
+              </Link>
           </span>
         </div>
         <button id="navbar-toggle" data-collapse-toggle="navbar-menu" type="button" 
@@ -52,11 +54,7 @@ function Header() {
         </button>
         <div className="hidden w-full md:flex md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-100 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 sm:bg-white md:bg-gray-100 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/">
-                Home
-              </Link>
-            </li>
+           
             {isAdmin && isLoggedIn && (
               <li>
                 <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/Dashboard/books">
